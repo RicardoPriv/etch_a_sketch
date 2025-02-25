380c1315a8a306d988270ca725fff8795b6eff70
Ricardo7720 <a103996@uminho.pt>
2025-02-25 00:34:43 +0000

    UI: - Added in a header - Added in a footer

--- script.js
+++ script.js
@@ -11,5 +11,13 @@ window.onload = function main() {
 
     sketch.appendChild(grid);
     sketch.style.display = "flex";
-    
+    sketch.style.gap = "16px";
+    sketch.style.justifyContent = "center";
+    sketch.style.alignItems = "center";
+
+    document.body.style.display = "flex";
+    document.body.style.flexDirection = "column";
+    document.body.style.justifyContent = "space-between";
+    document.body.style.alignItems = "center";
+    document.body.style.height = "100vh";
 }
